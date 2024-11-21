310c3ed2298b1b0e1e4b42be9ddea53d0e8ac49b
Sagar <desiferchef@gmail.com>
2024-11-21 14:04:59 +0545

    Add router.get routes

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 
 import { connectDB } from "./db/connectDB.js";
 
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 
 const app = express();
@@ -10,6 +12,8 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Whatever World");
 });
+
+app.unsubscribe("/api/auth", authRoutes);
 
 app.listen(3000, () => {
     connectDB();
