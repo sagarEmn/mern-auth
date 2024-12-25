09d7fc81d8aa4195e0339dc2f498e9ae5cb62eb1
Sagar <100994834+sagarEmn@users.noreply.github.com>
2024-12-25 22:25:36 +0545

    refactor: Make server port configurable using environment variable

--- backend/index.js
+++ backend/index.js
@@ -8,18 +8,11 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
-
-app.get("/home", (req, res) => {
-    res.send("This is home");
-})
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port: ", PORT);
 });
