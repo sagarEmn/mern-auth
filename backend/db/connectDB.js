e8536e6a07fe48c93bd22fa198e22edca33e6d71
Sagar <100994834+sagarEmn@users.noreply.github.com>
2024-12-25 21:37:55 +0545

    refactor: Remove logging of process.env

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -5,7 +5,6 @@ export const connectDB = async () => {
         console.log("mongo_uri: ", process.env.MONGO_URI);
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB Connected: ${conn.connection.host}`); 
-        console.log(process.env);
     } catch (error) {
         console.log("Error connection to MongoDB: ", error.message);
         process.exit(1);
