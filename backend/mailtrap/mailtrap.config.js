a1ba529ee2a8a824206dc960d3941a69998b2345
Sagar <100994834+sagarEmn@users.noreply.github.com>
2024-12-28 21:38:37 +0545

    feat(mailtrap): Set up Mailtrap integration for email testing

---
+++ backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,32 @@
+import { MailtrapClient } from "mailtrap";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log("MAILTRAP_TOKEN:", process.env.MAILTRAP_TOKEN);
+
+const TOKEN = process.env.MAILTRAP_TOKEN;
+
+const client = new MailtrapClient({
+  token: TOKEN,
+});
+
+const sender = {
+  email: "hello@demomailtrap.com",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "desiferchef@gmail.com",
+  }
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
