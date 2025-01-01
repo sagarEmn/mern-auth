9cf51933f77542084a84168bf4303a9a8657b355
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-01-01 22:21:25 +0545

    feat(mailtrap): add sendWelcomeEmail function

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -17,3 +17,25 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         throw new Error(`Error sending verification email: ${error}`);
     }
 };
+
+export const sendWelcomeEmail = async (email, name) => {
+    const recipient = [{ email }];
+
+    try {
+        const response = await MailtrapClient.send({
+            from: sendVerificationEmail,
+            to: recipient,
+            template_uuid: "...",
+            template_variables: {
+                company_info_name: "Auth Company",
+                name: name,
+            },
+        });
+
+        console.log("Welcome email sent successfully", response);
+    } catch(error) {
+        console.error(`Error sending welcome email`, error);
+
+        throw new Error(`Error sending welcome email: ${error}`);
+    }
+}
