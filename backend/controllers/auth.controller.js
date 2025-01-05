d61569e4d37bd84617e2746112ef217c199b6f30
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-01-05 19:37:53 +0545

    feat(mailtrap): Add forgot password email functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -90,15 +90,19 @@ export const login = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ success: false, message: "User's email not found" });
+      return res
+        .status(400)
+        .json({ success: false, message: "User's email not found" });
     }
     const isPasswordValid = await bcryptjs.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(400).json({ success: false, message: "Invalid credentials" });
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
     }
 
     generateTokenAndSetCookie(res, user._id);
-    
+
     user.lastLogin = new Date();
     await user.save();
 
@@ -120,6 +124,44 @@ export const logout = async (req, res) => {
   res.clearCookie("token");
   res.status(200).json({
     success: true,
-    message: "Logged out successfully"
+    message: "Logged out successfully",
   });
+};
+
+export const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+    }
+
+    // Generate reset token
+    const resetToken = crypto.randomBytes(20).toString("hex");
+    const resetTokenExpiresAt = Date.now() + 1 * 60 * 60 * 1000;
+
+    user.resetPasswordToken = resetToken;
+    user.resetPasswordExpiresAt = resetTokenExpiresAt;
+
+    await user.save();
+
+    // send email
+    await sendPasswordResetEmail(
+      user.email,
+      `${process.env.CLIENT_URL}/reset-password/${resetToken}`
+    );
+
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: "Password reset link sent to your email",
+      });
+  } catch (error) {
+    console.log("Error in forgotPassword ", error);
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -2,43 +2,64 @@ import { mailtrapClient, sender } from "./mailtrap.config.js";
 import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplate.js";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
-    const recipient = [{ email }];
+  const recipient = [{ email }];
 
-    try {
-        const response = await mailtrapClient.send({
-            from: sender,
-            to: recipient, 
-            subject: "Verify your email",
-            html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken),
-            category: "Email Verification",
-        });
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Verify your email",
+      html: VERIFICATION_EMAIL_TEMPLATE.replace(
+        "{verificationCode}",
+        verificationToken
+      ),
+      category: "Email Verification",
+    });
 
-        console.log("Email sent successfully", response);
-    } catch(error) {
-        console.error(`Error sending verification`, error);
+    console.log("Email sent successfully", response);
+  } catch (error) {
+    console.error(`Error sending verification`, error);
 
-        throw new Error(`Error sending verification email: ${error}`);
-    }
+    throw new Error(`Error sending verification email: ${error}`);
+  }
 };
 
 export const sendWelcomeEmail = async (email, name) => {
-    const recipient = [{ email }];
+  const recipient = [{ email }];
 
-    try {
-        const response = await mailtrapClient.send({
-            from: sender,
-            to: recipient,
-            template_uuid: "90a09282-a848-4508-ae6b-c7e2d5919cd0",
-            template_variables: {
-                company_info_name: "Auth Company",
-                first_name: name,
-            },
-        });
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "90a09282-a848-4508-ae6b-c7e2d5919cd0",
+      template_variables: {
+        company_info_name: "Auth Company",
+        first_name: name,
+      },
+    });
 
-        console.log("Welcome email sent successfully", response);
-    } catch(error) {
-        console.error(`Error sending welcome email`, error);
+    console.log("Welcome email sent successfully", response);
+  } catch (error) {
+    console.error(`Error sending welcome email`, error);
 
-        throw new Error(`Error sending welcome email: ${error}`);
-    }
-}
+    throw new Error(`Error sending welcome email: ${error}`);
+  }
+};
+
+export const sendPasswordResetEmail = async (email, name) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Reset your password",
+      html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+      category: "Password Reset",
+    });
+  } catch (error) {
+    console.error(`Error sending password reset email`, error);
+
+    throw new Error(`Error sending password reset email: ${error}`);
+  }
+};
