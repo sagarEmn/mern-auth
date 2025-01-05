d93c1bf11e7ec777e62e33e16edcdf9f97df7a9b
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-01-05 08:11:06 +0545

    feat(auth): Implement user login funcionality

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { signup, logout, verifyEmail } from "../controllers/auth.controller.js";
+import { signup, logout, login, verifyEmail } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/logout", logout);
+router.post("/login", login);
 
 router.post("/verify-email", verifyEmail);
 
