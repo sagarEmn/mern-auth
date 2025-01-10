c3149f97549143cab6be9d2b412a713c67174eca
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-01-10 20:37:44 +0545

    feat(frontend): Complete implementation of Floating Shape component

--- frontend/src/components/FloatingShape.jsx
+++ frontend/src/components/FloatingShape.jsx
@@ -1,7 +1,23 @@
-const FloatingShape = () => {
+import { motion } from "framer-motion";
+
+const FloatingShape = ({ color, size, top, left, dealay }) => {
     return(
-        <div>
-            FloatingShape
-        </div>
+        <motion.div 
+            className={`absolute rounded-full ${color} ${size} opacity-20 blur-xl ${top} ${left}`}
+            animate={{
+                y: ["0%", "100%", "0%"],
+                x: ["0%", "100%", "0%"],
+                rotate: [0, 360],
+            }}   
+            transition={{
+                duration: 20,
+                ease: "linear",
+                repeat: Infinity,
+                delay,
+            }}
+            aria-hidden='true'
+        />
     )
 }
+
+export default FloatingShape;
