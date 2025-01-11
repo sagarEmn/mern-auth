a313d12a6bddb3ac02f9a52ed1bc1c0b32b4b56d
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-01-11 19:46:23 +0545

    fix(frontend): Fix a spelling mistake for prop delay

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import FloatingShape from "./components/FloatingShape";
 
 function App() {
   return (
--- frontend/src/components/FloatingShape.jsx
+++ frontend/src/components/FloatingShape.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 
-const FloatingShape = ({ color, size, top, left, dealay }) => {
+const FloatingShape = ({ color, size, top, left, delay }) => {
     return(
         <motion.div 
             className={`absolute rounded-full ${color} ${size} opacity-20 blur-xl ${top} ${left}`}
