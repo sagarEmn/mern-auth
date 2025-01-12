dc031e182b1f95d03654a2e7169f01779763745a
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-01-12 22:25:30 +0545

    feat(frontend): Add signup page components, initialize Input.jsx for input elements

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,8 +1,25 @@
+import { motion } from "framer-motion";
+
 const SignUpPage = () => {
+  const handleSignUp = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <>
-      <div>SignUpPage</div>
-    </>
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+    >
+      <div className="p-8">
+        <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-4 from-green-400 to-emerald-500 text-transparent bg-clip-text">
+          Create your account
+        </h2>
+
+        <form onSubmit={handleSignUp}></form>
+      </div>
+    </motion.div>
   );
 };
 
