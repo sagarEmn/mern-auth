4dacd072fd1116efb0e02e460ec90c16631e6c31
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-12 16:48:09 +0545

    feat: Add MongoDB database connection configuration
    - Implement databse connection function using mongoose
    - Add error handling for connection failures
    - Add environment variables for MongoDB URI

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,5 +13,6 @@ app.get("/", (req, res) => {
 })
 
 app.listen(PORT, () => {
+    connectDB();
     console.log("Server is running on port 5000");
 })
