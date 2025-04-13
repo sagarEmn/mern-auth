809f55dbe3c366f34b8c7171ef63c8a01ccdbec5
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-13 22:51:32 +0545

    WIP: Implement initial user signup logic

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,43 @@
+import { User } from "../models/user.model";
+import bcryptjs from "bcryptjs";
+
 export const signup = async (req, res) => {
-    res.send("signup page");
+  const { email, password, name } = req.body;
+
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All fields are required");
+    }
+
+    const userAlreadyExists = await User.findOne({ email });
+    console.log("userAlreadyExists", userAlreadyExists);
+
+    if (userAlreadyExists) {
+        return res.status(400).json({success: false, message: "User already exists"});
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+
+    const user = new User({
+      email, 
+      password: hashedPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // 24 hours
+    });
+
+    await user.save();
+    
+  } catch (error) {
+
+  }
 };
 
 export const login = async (req, res) => {
-    res.send("login page");
+  res.send("login page");
 };
 
 export const logout = async (req, res) => {
-    res.sesnd("logout page");
+  res.sesnd("logout page");
 };
