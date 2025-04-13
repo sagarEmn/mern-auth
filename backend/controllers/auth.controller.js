a5c59c2bea67581857bd7477d2ac8c5d61bc748a
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-13 14:45:14 +0545

    feat: Create User model schema
    - Define MongoDB schema for User collection
    - Add fields: email, password, name, lastLogin, isVerified
    - Add tokens and expiration for tokens for reset password and verification tokens

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+    res.send("signup page");
+};
+
+export const login = async (req, res) => {
+    res.send("login page");
+};
+
+export const logout = async (req, res) => {
+    res.sesnd("logout page");
+};
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,17 +1,12 @@
 import express from "express";
+import { login, signup, logout } from "../controllers/auth.controller";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-    res.send("signup route");
-});
+router.get("/signup", signup);
 
-router.get("/login", (req, res) => {
-    res.send("login route");
-});
+router.get("/login", login);
 
-router.get("/logout", (req, res) => {
-    res.send("logout route");
-});
+router.get("/logout", logout);
 
 export default router;
