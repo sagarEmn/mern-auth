2787f619f61664cb0366c71855c7dc8bf4e42946
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-18 20:32:46 +0545

    feat(email): Create sendVerificationEmail function using Mailtrap
    - takes recipient email and verificationToken as arguments
    - imports the Mailtrap client and the verification HTML template
    - uses  to send the email

---
+++ mailtrap/emails.js
@@ -0,0 +1,25 @@
+import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+import { mailtrapClient } from "./mailtrap.config.js";
+
+export const sendVerificationEmail = async (email, verificationToken) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "VERIFY YOUR EMAIL",
+      html: VERIFICATION_EMAIL_TEMPLATE.replace(
+        "{VerificationCode}",
+        verificationToken
+      ),
+      category: "Email Verification",
+    });
+
+    console.log("Verification Code Email sent successfully", response);
+  } catch (error) {
+    console.error(`Error sending verification email`, error);
+
+    throw new Error(`Error sending verification email: ${email}`);
+  }
+};
