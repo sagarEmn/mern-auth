e080773f98721ecf5d579b0afb7f82d2880732be
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-18 20:58:10 +0545

    fix(mailtrap): Fix the verification email placeholder and import sender

--- mailtrap/emails.js
+++ mailtrap/emails.js
@@ -1,5 +1,5 @@
 import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
-import { mailtrapClient } from "./mailtrap.config.js";
+import { mailtrapClient, sender } from "./mailtrap.config.js";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
   const recipient = [{ email }];
@@ -10,13 +10,14 @@ export const sendVerificationEmail = async (email, verificationToken) => {
       to: recipient,
       subject: "VERIFY YOUR EMAIL",
       html: VERIFICATION_EMAIL_TEMPLATE.replace(
-        "{VerificationCode}",
+        "{verificationCode}",
         verificationToken
       ),
       category: "Email Verification",
     });
 
     console.log("Verification Code Email sent successfully", response);
+    console.log("Verification code: ", verificationToken);
   } catch (error) {
     console.error(`Error sending verification email`, error);
 
