0077ab45bce68fae9e617f7faf41161dba3114c8
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-18 17:46:35 +0545

    refactor(mailtrap: Export configured token, mailtrapClient and remove test send logic
    - remove sender
    - remove recepients object
    - remove send logic

--- mailtrap/mailtrap.config.js
+++ mailtrap/mailtrap.config.js
@@ -5,28 +5,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // access the token from process.env
-const TOKEN = process.env.MAILTRAP_TOKEN;
+export const TOKEN = process.env.MAILTRAP_TOKEN;
 
-const client = new MailtrapClient({
+export const mailtrapClient = new MailtrapClient({
   token: TOKEN,
 });
 
-const sender = {
-  email: "hello@demomailtrap.com",
-  name: "Mailtrap Test",
-};
-const recipients = [
-  {
-    email: "desiferchef@gmail.com",
-  },
-];
-
-client
-  .send({
-    from: sender,
-    to: recipients,
-    subject: "You are awesome!",
-    text: "Congrats for sending test email with Mailtrap!",
-    category: "Integration Test",
-  })
-  .then(console.log, console.error);
