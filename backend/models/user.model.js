4b9d16b9d8339b90fbd24817f3b6b409d174377f
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-19 22:52:12 +0545

    fix(db): Fix a typo

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema(
     resetPasswordToken: String,
     resetPasswordExpiresAt: Date,
     verificationToken: String,
-    VerificationTokenExpiresAt: Date,
+    verificationTokenExpiresAt: Date,
   },
   { timestamps: true }
 );
