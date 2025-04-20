5e955fc19e09bd23c612e646f97d8d3144cee172
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-20 16:31:34 +0545

    feat(auth): Add user login function that logs in the user
    - Uses login controller for login function
    - Handles user lookup by email
    - Compares the provided password with stored hash using bcryptjs
    - Updates 'lastLogin' field on successful authentication
    - Returns basic status update to the client based on try-catch error handling

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -98,9 +98,50 @@ export const verifyEmail = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("login page");
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false, 
+        message: "Invalid credentials"
+      });
+    }
+
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.stauts(400).json({
+        success: false, 
+        message: "Invalid credentials",
+      });
+    }
+
+    user.lastLogin = new Date();
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      mesage: "Logged in successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log("Error in login: ", error);
+    res.status(400).json({
+      success: false, 
+      message: error.message
+    });
+  }
 };
 
 export const logout = async (req, res) => {
-  res.send("logout page");
+  res.clearCookie("token");
+  res.status(200).json({
+    success: true, 
+    message: "Logged out successfully"
+  })
 };
