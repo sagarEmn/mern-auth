3f6b5e5b1011ca9bb58fffd880c621c408853520
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-20 14:40:46 +0545

    feat(mailtrap): Add send welcome email function using Mailtrap
    Implement the sendWelcomeEmail funciton to send a welcome message to newly verified users via Mailtrap API

--- mailtrap/emails.js
+++ mailtrap/emails.js
@@ -23,3 +23,24 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     throw new Error(`Error sending verification email: ${email}`);
   }
 };
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "bee23565-367c-4bca-a0ac-403bb829fabf",
+      template_variables: {
+        company_info_name: "Auth Company",
+        name: name, 
+      }
+    })
+
+    console.log("Welcome email sent successfully: ", response);
+  } catch (error) {
+    console.error(`Error sending welcome email:`, error);
+    throw new Error(`Error sending welcome email: ${error}`);
+  }
+}
