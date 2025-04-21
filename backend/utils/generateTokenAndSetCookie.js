c80a51f663e9fa9058e2067fe6b104d08f33d98d
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-21 09:56:38 +0545

    docs(cookie): Add explanation to token generation and res.cookie function

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -4,6 +4,10 @@ export const generateTokenAndSetCookie = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: "7d",
     });
+    // token contains: 
+    // - encoded header (info about the token type & signing algorithm)
+    // - encoded payload ({ userId })
+    // - signature (created using header, payload, JWT_SECRET)
 
     res.cookie("token", token, {
         httpOnly: true, 
@@ -11,6 +15,11 @@ export const generateTokenAndSetCookie = (res, userId) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         sameSite: "Strict", 
     });
+    // res.cookie places the cookie named "token" with the JWT string as it's value into the HTTP response header's(Set-Cookie) header
+    // this response is sent back only to the specific browser that initiated the request
+    // eg: the browser where the user just signed up or logged in
+    // the user's browser receives this response and automatically stores the cookie, associates this cookie with your website's domain
+    // From then on, the browser will automatically include this cookie in future requests from that user to your server
 
     return token;
 }
