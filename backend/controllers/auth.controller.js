278fe544059a4df7fd3935d6de94925e5e072cb1
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-23 15:58:41 +0545

    feat(auth): Create route handler function for resetting password

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -182,7 +182,7 @@ export const forgotPassword = async (req, res) => {
     const user = await User.findOne({ email });
 
     if (!user) {
-      res.status(400).json({
+      return res.status(400).json({
         success: false,
         message: "User doesn't exist",
       });
@@ -212,6 +212,46 @@ export const forgotPassword = async (req, res) => {
     res.status(400).json({
       success: false,
       message: "Error in forgotPassword",
+    });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  try {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpiresAt: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid or reset password token expired",
+      });
+    }
+
+    // update password
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    user.password = hashedPassword;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpiresAt = undefined;
+    await user.save();
+
+    await sendResetSuccessEmail(user.email);
+
+    res.status(200).json({
+      success: true,
+      message: "Password reset successful",
+    });
+  } catch (error) {
+    console.log("Error in resetPassword: ", error);
+    res.status(400).json({
+      success: false,
+      message: error.message,
     });
   }
 };
