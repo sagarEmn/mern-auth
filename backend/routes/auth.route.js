74ee7f97d45cb18230f1446f33e51983fb747bad
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-23 15:59:21 +0545

    feat(routes): Add reset password route handler to the route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, signup, logout, verifyEmail, forgotPassword, checkAuth } from "../controllers/auth.controller.js";
+import { login, signup, logout, verifyEmail, forgotPassword, checkAuth, resetPassword } from "../controllers/auth.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
@@ -11,6 +11,8 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.post("/verify-email", verifyEmail);
+
 router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 export default router;
