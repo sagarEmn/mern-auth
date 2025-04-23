dc9bb2e8683b73630c79602879bbcd9d0a8b5c65
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-23 15:08:00 +0545

    feat(auth): Add middleware for /check-auth route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { login, signup, logout, verifyEmail, forgotPassword } from "../controllers/auth.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/check-auth", verifyToken);
 
 router.post("/signup", signup);
 router.post("/login", login);
