f63ba4d7e4c17d0b570c31aed986fc456eb9dd43
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-25 21:46:26 +0545

    WIP: Add password strength meter

--- frontend/src/components/PasswordStrengthMeter.jsx
+++ frontend/src/components/PasswordStrengthMeter.jsx
@@ -17,7 +17,7 @@ const PasswordCriteria = ({ password }) => {
 
   return (
     <div className="mt-2 space-y-1">
-      {criteria.map((item, index) => (
+      {criteria.map((item) => (
         <div key={item.label} className="flex items-center text-xs">
           {item.met ? (
             <Check className="size-4 text-green-500 mr-2" />
@@ -63,6 +63,36 @@ const PasswordStrengthMeter = () => {
     if (strength == 3) return "Good";
     return Strong;
   };
+
+  const getColor = (strength) => {
+    if (strength == 0) return "bg-red-500";
+    if (strength == 1) return "bg-red-400";
+    if (strength == 2) return "bg-yellow-500";
+    if (strength == 3) return "bg-yellow-400";
+    return "bg-green-500";
+  };
+
+  return (
+    <div className="mt-2">
+      <div className="flex justify-center items-center mb-1">
+        <span className="text-xs text-gray-400">Password Strength</span>
+        <span className="text-xs text-gray-400">
+          {getStrengthText(strength)}
+        </span>
+      </div>
+
+      <div className="flex space-x-1">
+        {[...Array(4)].map((_, index) => (
+          <div
+            className={`h-1 w-1/4 rounded-full transition-colors duration-300
+            ${index < strength ? getColor(strength) : "bg-gray-600"}`}
+          ></div>
+        ))}
+      </div>
+
+      <PasswordCriteria password={password} />
+    </div>
+  );
 };
 
 export default PasswordStrengthMeter;
