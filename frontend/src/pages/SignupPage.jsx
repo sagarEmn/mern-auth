a117f891d136be57b24b184d6ab220a0e2b816d3
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-25 16:07:39 +0545

    refactor: Improve semantic HTML in SignupPage

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Loader, Lock, Mail, User } from "lucide-react";
 import { motion } from "framer-motion";
+import { Link } from "react-router";
 
 // import components
 import Input from "../components/Input";
@@ -13,21 +14,21 @@ export const SignupPage = () => {
 
   const handleSignup = (e) => {
     e.preventDefault();
-
-    
   };
 
   return (
-    <motion.div
+    <motion.article
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
       className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
     >
-      <main className="p-8">
-        <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
-          Create An Account
-        </h2>
+      <div className="p-8">
+        <header>
+          <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
+            Create An Account
+          </h2>
+        </header>
 
         <form onSubmit={handleSignup}>
           <Input
@@ -37,24 +38,43 @@ export const SignupPage = () => {
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
-          <Input 
+          <Input
             icon={Mail}
             type="email"
             placeholder="Email Address"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
-          <Input 
+          <Input
             icon={Lock}
             type="password"
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-
-
+          <motion.button
+            className="mt-5 w-full py-3 px-4 bg-gradient-to-r 
+          from-green-500 to-emerald-600 
+          text-white font-bold 
+          rounded-lg shadow-lg 
+          hover:from-green-600 hover:to-emerald-700
+          focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition"
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            type="submit"
+          >
+            Signup
+          </motion.button>
         </form>
-      </main>
-    </motion.div>
+      </div>
+      <footer className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
+        <p className="text-sm text-gray-400">
+          Already have an account?{" "}
+          <Link className="text-green-400 hover:underline" to="/login">
+            Login
+          </Link>
+        </p>
+      </footer>
+    </motion.article>
   );
 };
