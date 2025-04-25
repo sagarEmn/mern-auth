a241cca60e4f90506b6379f414657939c02c147f
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-04-25 16:56:48 +0545

    feat: Add password strength lables array and function to map onto that array

---
+++ frontend/src/components/PasswordStrengthMeter.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+// lucide-react imports
+import { Check } from "lucide-react";
+
+const PasswordCriteria = ({ password }) => {
+  const criteria = [
+    { label: "At least 6 characters", met: password.length >= 6 },
+    { label: "Contains an uppercase letter", met: /[A-Z]/.test(password) },
+    { label: "Contains a lowercase letter", met: /[a-z]/.test(password) },
+    { label: "Contains a number", met: /\d/.test(password) },
+    {
+      label: "Contains a special character",
+      met: /[A-Za-z0-9]/.test(password),
+    },
+  ];
+
+  return (
+    <div className="mt-2 space-y-1">
+      {criteria.map((item, index) => (
+        <div key={item.label} className="flex items-center text-xs">
+          {item.met ? (
+            <Check className="size-4 text-green-500 mr-2" />
+          ) : (
+            <X className="size-4 text-gray-500" />
+          )}
+          <span className={item.met ? "text-green-500" : "text-gray-500"}>
+            {item.label}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const PasswordStrengthMeter = () => {
+  return <>PasswordStrengthMeter</>;
+};
+
+export default PasswordStrengthMeter;
