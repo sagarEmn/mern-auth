7c3a4d658d52246a28bc97c39468f908ef665d74
unknown <desiferchef@gmail.com>
2025-05-10 21:52:27 +0545

    WIP: Add OTP UI

--- frontend/src/pages/EmailVerificationPage.jsx
+++ frontend/src/pages/EmailVerificationPage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const EmailVerificationPage = () => {
   const [code, setCode] = useState(["", "", "", "", "", ""]);
+  const inputRefs = userRef([]);
+  const navigate = useNavigate();
+
+  const handleChange = (index, value) => {
+    const newCode = [...code];
+
+    // handle pasted contents
+    if (value.length > 1) {
+      const pastedCode = value.slice(0, 6).split("");
+      for (let i = 0; i < 6; i++) {
+        newCode[i] = pastedCode[i] || "";
+      }
+      setCode(newCode);
+    }
+  }
 
   return (
     <div>EmailVerificationPage</div>
