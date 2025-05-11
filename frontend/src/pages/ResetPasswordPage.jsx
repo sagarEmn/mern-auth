70250ab02471270775f6a8543bb640a1c1ab4cdb
unknown <desiferchef@gmail.com>
2025-05-12 05:22:31 +0545

    feat: Implement basic Reset Password Page

--- frontend/src/pages/ResetPasswordPage.jsx
+++ frontend/src/pages/ResetPasswordPage.jsx
@@ -40,7 +40,39 @@ const ResetPasswordPage = () => {
                 </h2>
                 {/* display error */}
                 {/* display message */}
+
+                <form onSubmit={handleSubmit}>
+                    <Input
+                        icon={Lock}
+                        type="password"
+                        placeholder="New Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+
+                    <Input 
+                        icon={Lock}
+                        type="password"
+                        placeholder="Confirm New Password"
+                        value={password}
+                        onChange={(e) => {setConfirmPassword(e.target.value)}}
+                        required
+                    />
+
+                    <motion.button
+                        whileHover={{ scale: 1.02 }}
+                        whileTap={{ scale: 0.98 }}
+                        className="w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white fron-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
+                        type="submit"
+                        disabled={isLoading}
+                    >
+                        {/* Loader component from authstore */}
+                    </motion.button>
+                </form>
             </div>
         </motion.div>
     )
 }
+
+export default ResetPasswordPage;
