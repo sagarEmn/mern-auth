d96387edb7155b33d11a161fc740adeb57e5791c
unknown <desiferchef@gmail.com>
2025-05-12 04:47:21 +0545

    feat: Add basic ForgotPasswordPage components

---
+++ frontend/src/pages/ForgotPasswordPage.jsx
@@ -0,0 +1,29 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+import Input from "../components/Input";
+import { ArrowLeft, Loader, Mail } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const ForgotPasswordPage = () => {
+    const [email, setEmail] = useState("");
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    };
+
+    return(
+        <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+        >
+            <div className="p-8">
+                
+            </div>
+
+        </motion.div>
+    )
+
+}
