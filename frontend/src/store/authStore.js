be42fe2f2c64bea7118068095d5a8fa96243c33a
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-05-13 21:05:57 +0545

    feat: Implement check authentication function in auth store for state management

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -75,5 +75,15 @@ export const useAuthStore = create((set) => ({
       set({ error: error.response.data.message || "Error verifying email", isLoading: false });
       throw error;
     }
-  }
+  },
+
+  checkAuth: async () => {
+    set({ isCheckingAuth: true, error: null });
+    try {
+      const response = await axios.get(`${API_URL}/check-auth`);
+      set({ user: response.data.user, isAuthenticated: true, isCheckingAuth: false });
+    } catch (error) {
+      set({ error: null, isCheckingAuth: false, isAuthenticated: false });
+    }
+  },
 }));
