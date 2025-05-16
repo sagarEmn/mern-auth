fd4db1c75c26752fc0deda94edb568db80062af0
unknown <desiferchef@gmail.com>
2025-05-16 12:02:56 +0545

    feat: Add Loader component based on a condition

--- frontend/src/pages/ForgotPasswordPage.jsx
+++ frontend/src/pages/ForgotPasswordPage.jsx
@@ -46,7 +46,11 @@ const ForgotPasswordPage = () => {
               className="w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
               type="submit"
             >
-              Loader Component from authstore
+              {isLoading ? (
+                <Loader className="size-6 animate-spin mx-auto" />
+              ) : (
+                "Send Reset Link"
+              )}
             </motion.button>
           </form>
         ) : (
