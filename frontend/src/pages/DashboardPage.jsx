44eb12626f83c74130f04a95e58d19e2cdaf210d
unknown <desiferchef@gmail.com>
2025-05-19 11:05:53 +0545

    feat(frontend): Implement basic dashboard page

---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,13 @@
+import { motion } from "framer-motion";
+import { useAuthStore } from "../store/authStore";
+import { formatDate } from "../utils/date.js";
+
+const DashboardPage = () => {
+    return (
+        <>
+            DashboardPage
+        </>
+    )
+}
+
+export default DashboardPage;
