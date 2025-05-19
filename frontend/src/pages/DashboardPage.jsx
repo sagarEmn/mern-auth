3bddb6fd6a3c5c7cd49c1f32825868c177435d91
unknown <desiferchef@gmail.com>
2025-05-19 11:24:57 +0545

    feat: Use user, logout to implement logout functionality
    - Extract user and logout from useAuthStore
    - Add handleLogout function and use logout function to implement logout functionality

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -3,6 +3,14 @@ import { useAuthStore } from "../store/authStore";
 import { formatDate } from "../utils/date.js";
 
 const DashboardPage = () => {
+
+    // extract/desctructure "user" and "logout" from the object returned by useAuthStore()
+    const { user, logout } = useAuthStore();
+
+    const handleLogout = () => {
+        logout();
+    }
+
     return (
         <>
             DashboardPage
