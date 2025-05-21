d520863df4bae592b8d73497b14d2265f9c50a8b
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-05-21 12:36:23 +0545

    feat: Implement basici dashboard page UI
    - Adds the basic structure with header title as dashboard
    - Uses motion.div to stylize the component

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,21 +1,30 @@
+import React from "react";
+
 import { motion } from "framer-motion";
 import { useAuthStore } from "../store/authStore";
 import { formatDate } from "../utils/date.js";
 
 const DashboardPage = () => {
+  // extract/desctructure "user" and "logout" from the object returned by useAuthStore()
+  const { user, logout } = useAuthStore();
 
-    // extract/desctructure "user" and "logout" from the object returned by useAuthStore()
-    const { user, logout } = useAuthStore();
+  const handleLogout = () => {
+    logout();
+  };
 
-    const handleLogout = () => {
-        logout();
-    }
-
-    return (
-        <>
-            DashboardPage
-        </>
-    )
-}
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.9 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-md w-full mx-auto mt-10 p-8 bg-gray-900 bg-opacity-80 backdrop-filter backdrop-blur-lg rounded-xl shadow-2xl border border-gray-800"
+    >
+      <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-600 text-transparent bg-clip-text">
+        Dashboard
+      </h2>
+    </motion.div>
+  );
+};
 
 export default DashboardPage;
